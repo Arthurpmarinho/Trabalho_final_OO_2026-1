35885bf4795203fdd76ab52afd3861a780dc0045
r2phantomjoker <Arthurpinheiromarinho@gmail.com>
2026-06-21 19:31:08 -0300

    feat: criação da exceção EstoqueInsuficienteException

--- cafeteria/src/br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
+++ cafeteria/src/br/edu/cafeteria/excecao/EstoqueInsuficienteException.java
@@ -1,7 +1,6 @@
 package br.edu.cafeteria.excecao;
 
-public class EstoqueInsuficienteException extends Exception{
-
+public class EstoqueInsuficienteException extends Exception {
     public EstoqueInsuficienteException(String message) {
         super(message);
     }
